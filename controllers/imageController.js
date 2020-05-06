82bd9db3d864534600d4e27289dcf740cc1ac5ad
jinsung_cho <cjs97111@gmail.com>
2020-05-07 00:52:44 +0900

    Add : controller

---
+++ controllers/imageController.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const crypto = require("crypto");
+const fs = require('fs');
+const fsp = require('fs').promises;
+const rimraf = require('rimraf');
+
+const models = require("../models");
+const salt = require('../config/configs').salt;
+const base_path = require('../config/configs').base_path;
+const hash = require('../config/configs').hash;
+const dataset_dir_name = require('../config/configs').datasets;
+const res_handler = require('./responeHandler');
+
+module.exports = {
+    sendClassImage(req, res){
+        //image 썸네일 보기
+        
+    },
+
+    uploadImage(req ,res){
+        const hashId = crypto.createHash(hash).update(req.params.id + salt).digest("hex");
+        const dataset_path = `${base_path}/${hashId}/${data_dir_name}/${req.body.dataset_name}`;
+        var add_file_cnt = req.files.length;
+
+        console.log(req.files);
+
+        if(!add_file_cnt){
+            res.status(200).json({
+                success : "false",
+                massege : "이미지를 선택해주세요"
+            })
+        }
+    },
+
+    deleteImage(req, res){
+
+    },
+}
